8a39073335ff3d7b7e493de22cd8fe1062e0bb11
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2022-01-04 21:06:16 +0100

    Remove docker and shell scripts, reorder dir structure

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -r jspsych-6.3.1 local-server/webroot
-cp -r src/* local-server/webroot
-cp -r media local-server/webroot
-cp config.js local-server/webroot
-
-mv local-server/webroot/experiment.html local-server/webroot/index.html
